6ffc5969195fc6bbf39ca72149c784154eecf574
Steve Nelson <steve.nelson@realitybridges.com>
2013-05-06 01:39:22 -0400

    testing

--- server/app.js
+++ server/app.js
@@ -1,34 +1,11 @@
 var App=function(){
 	var self=this;
-	this.port=8080;
-	this.name='ZenmanEnergy';
-	
-	var express = require('express');
-	
-	
-	var app = express.createServer();
-	
-	app.configure(function() {
-	    app.use(express.static(__dirname + "/public"));
-	    app.set('views', __dirname);
-	    app.set('view engine', 'ejs');
-	});
-	app.get('/', function(req, res) {
-	    res.render('index', { layout: false });
-	});
-	app.listen(this.port);
-	
-	
-	var wsServer = new WebSocketServer({
-	    httpServer: app,
-	    
-	    // Firefox 7 alpha has a bug that drops the
-	    // connection on large fragmented messages
-	    fragmentOutgoingMessages: false
-	});
-	
-	
-	console.log("Zenman Server ready");
+	var http = require('http');
+	http.createServer(function (req, res) {
+	  res.writeHead(200, {'Content-Type': 'text/plain'});
+	  res.end('Hello World\n');
+	}).listen(1337);
+	console.log('Server running at http://127.0.0.1:1337/');
 }
 	
 var app=new App();
