78fcdefbc93a7df3544f899b3cfc0ccd799d2179
Steve Nelson <steve.nelson@realitybridges.com>
2013-06-05 16:50:02 -0400

    first create

--- client/js/reflector.js
+++ client/js/reflector.js
@@ -70,11 +70,112 @@ function Reflector(canvasId){
 		}
 		
 	}
+	
+	
+	self.findParabola=function(linearLength){
+		
+		var lastX=0
+		var lastY=0
+		var x=0
+		var y=0
+		var found=0;
+		var HALFLinearLength=linearLength/2
+		var currentHALFLinearLength=0
+		var lastHALFLinearLength=0
+		var distance=0;
+		var increment=linearLength/1000;
+		var increment=parseFloat(increment.toFixed(2));
+		//linearLength/10
+		var parabola=new Object
+		parabola.arcLength=linearLength;
+		for (var focalPointY=1; focalPointY <= linearLength/3; focalPointY+=1){
+			currentHALFLinearLength=0;
+			found=0;
+			var lastX=0, lastY=0;
+			var counter=1;
+			parabola.focalPointX=0
+			parabola.focalPointY=focalPointY
+			parabola.x=new Array;
+			parabola.y=new Array;
+			for(var x=1; x <= HALFLinearLength; x=parseFloat(x+increment)){
+				x=parseFloat(x.toFixed(2))
+				y=(x*x) / (4*focalPointY)
+				y=parseFloat(y.toFixed(2))
+				parabola.x.push(x)
+				parabola.y.push(y)
+				distance=Math.sqrt(((x-lastX)*(x-lastX)) + ((y-lastY)*(y-lastY)))
+				distance=parseFloat(distance)
+				
+				lastHALFLinearLength=currentHALFLinearLength
+				currentHALFLinearLength=parseFloat(currentHALFLinearLength)+parseFloat(distance)
+				
+				if (currentHALFLinearLength >= HALFLinearLength){
+					found=1;
+					break;
+				} else if (y >= focalPointY){
+					break;
+					found=0;
+				}
+				
+				lastX=x;
+				lastY=y;
+				counter++
+			}
+			if (found){break};
+		}
+		return parabola;
+	}
+	
+	
+	
+	self.setPoints=function(){
+		var parabola=self.findParabola(10);
+		self.x=new Array;
+		self.y=new Array;
+		var range=self.canvas.width
+		var middle=range/2
+		var tick=(self.canvas.width/10)
+		console.log(middle)
+		for( var i =parabola.x.length-1; i > 0 ; i--){
+			self.x[self.x.length]=middle-(tick*parabola.x[i])
+			self.y[self.y.length]=range-(middle+(tick*parabola.y[i]));
+			
+		}
+		for( var i =0; i < parabola.x.length; i++){
+			self.x[self.x.length]=middle+(tick*parabola.x[i])
+			self.y[self.y.length]=range-(middle+(tick*parabola.y[i]));
+		}
+		
+		
+	}
+	self.rotatePoints=function(cx,cy,angle){
+		
+		for( var i =0; i < self.x.length;i++){
+			
+			
+			s = Math.sin(angle);
+			c = Math.cos(angle);
+			
+			  // translate point back to origin:
+			self.x[i] -= cx;
+			self.y[i] -= cy;
+			
+			  // rotate point
+			xnew = self.x[i] * c - self.y[i]* s;
+			ynew = self.x[i] * s + self.y[i] * c;
+			
+			self.x[i] = xnew + cx;
+			self.y[i] = ynew + cy;
+		}
+	}
 	self.draw=function(){
 		var x,y;
 		var range=self.canvas.width
 		var middle=range/2
 		var tick=(self.canvas.width/10)
+		
+		
+		
 		self.context.lineWidth = 1;
 		
 		//draw cross at the center of the canvas
@@ -102,98 +203,14 @@ function Reflector(canvasId){
 			self.context.stroke();
 		}
 		
-		x0=range/2
-		y0=range/2
-		self.context.lineWidth = 3;
-		
-		offset = range/2
-		console.log(range,middle,tick)
-		
-		self.x=new Array;
-		self.y=new Array;
-		for( var x =0; x < 5; x++){
-			y=  (x * x)/ (4 * 5)
-			console.log(y)
-			self.x[x]=middle+x*tick
-			self.y[x]=middle+y*tick
-		}
-		console.log(self.x)
-		
 		self.context.beginPath();
+		self.context.lineWidth = 5;
 		self.context.moveTo(self.x[0],self.y[0]);
-		for (var i = 1; i < self.x.length; i++) {
-			console.log(self.x[i])
+		for( var i =1; i < self.x.length; i++){
 			self.context.lineTo(self.x[i],self.y[i]);
 		}
 		self.context.stroke();
 		
-		/*
-		 * 
-		
-		self.context.strokeStyle = '#008000';
-		for( var i =0; i < 4; i++){
-			
-			startX=x0+(i*tick)
-			startY=startX
-			endX=x0+((1+i)*tick)
-			endY=endX
-			
-			self.context.beginPath();
-			self.context.moveTo(startX,startY);
-			self.context.lineTo(endX,endY);
-			self.context.stroke();
-			
-
-		}
-		
-		 */
-		
-		
-		
-		
-		
-		/*
-		 * self.context.beginPath();
-		self.context.moveTo(self.canvas.width/2, self.canvas.height/2);
-		self.context.lineTo(1000,900);
-		self.context.lineWidth = 2;
-		self.context.strokeStyle = '#ff0000';
-		self.context.stroke();
-		
-		
-		self.context.beginPath();
-		self.context.moveTo(self.canvas.width/2, self.canvas.height/2);
-		var a=-1;
-		var b=-500;
-		var c=0
-		
-		
-		console.log(range)
-		console.log(tick)
-		for (var t=0; t < 50; t++){
-			
-			x=range/2 + tick * t;
-			//range - 
-			y=range/2 + 0.001*Math.pow(x,2);
-			//y=(0.002*Math.pow(x,2));
-			//y = (a * Math.pow(x,2)) + (b * x) + c;
-			//y = -1*Math.pow((x+10),2);
-			console.log(x,y)
-			self.context.lineTo(x, y);
-		}
-		self.context.lineWidth = 2;
-		self.context.strokeStyle = '#ff0000';
-		self.context.stroke();
-		    
-		
-		
-		for (var i = 0; i < self.x.length; i++) {
-		//	console.log(i, self.x[i])
-			self.context.lineTo(self.x[i],self.y[i]);
-		}
-		self.context.stroke();
-		
-		 */
 	}
 	
 	
